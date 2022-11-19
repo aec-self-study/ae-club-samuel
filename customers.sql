5eab404b45e9a8f034c7b2138d94c68a92072592
Samuel Budlong <73445416+SamtheDeveloper91@users.noreply.github.com>
2022-11-19 23:16:24 +0000

    create customers.sql

---
+++ customers.sql
@@ -0,0 +1,29 @@
+with customers as (
+
+  select 
+    id as customer_id, 
+    name as customer_name, 
+    email as customer_email 
+  from `analytics-engineers-club.coffee_shop.customers`
+
+), orders as (
+  
+  select 
+    customer_id, 
+    min(created_at) as first_order_date, 
+    count(*) as count_orders 
+  from `analytics-engineers-club.coffee_shop.orders` 
+  group by 1
+  
+)
+
+select
+  customers.customer_id,
+  customers.customer_name,
+  customers.customer_email,
+  orders.first_order_date,
+  orders.count_orders
+from customers
+left join orders
+  on customers.customer_id = orders.customer_id
+order by first_order_date
