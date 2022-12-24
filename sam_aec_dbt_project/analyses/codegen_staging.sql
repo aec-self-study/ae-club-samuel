7bd5b716d9965850147062a0aed8312f3363d5ee
Samuel Budlong <73445416+SamtheDeveloper91@users.noreply.github.com>
2022-12-24 16:50:41 +0000

    stage pageviews

--- sam_aec_dbt_project/analyses/codegen_staging.sql
+++ sam_aec_dbt_project/analyses/codegen_staging.sql
@@ -1,5 +1,5 @@
 {{ codegen.generate_base_model(
-    source_name='coffee_shop',
-    table_name='order_items',
+    source_name='web_tracking',
+    table_name='pageviews',
     materialized='view'
 ) }}
---
+++ sam_aec_dbt_project/models/staging/sources/web_tracking/stg_web_tracking__pageviews.sql
@@ -0,0 +1,20 @@
+with pageviews_source as (
+
+    select * from {{ source('web_tracking', 'pageviews') }}
+
+),
+
+renamed as (
+
+    select
+        id as pageview_id,
+        customer_id,
+        visitor_id,
+        device_type,
+        timestamp as pageview_timestamp,
+        page as page_name
+    from source
+
+)
+
+select * from renamed
